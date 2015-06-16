7bcc8e788c3e0d414f73e8de17f0f2876be92742
ganchau <gancchau@gmail.com>
2015-06-16 14:45:50 -0400

    lab complete minus extra credit

---
+++ locationTrivia-tableviews/FISLocationsTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FISLocationsTableViewController.h
+//  locationTrivia-tableviews
+//
+//  Created by Gan Chau on 6/16/15.
+//  Copyright (c) 2015 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISLocationsTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *locations;
+
+@end
---
+++ locationTrivia-tableviews/FISTriviaTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FISTriviaTableViewController.h
+//  locationTrivia-tableviews
+//
+//  Created by Gan Chau on 6/16/15.
+//  Copyright (c) 2015 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISTriviaTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *trivia;
+
+@end
